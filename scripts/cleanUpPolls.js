13799a6861f9f881659e22c40947e2f95876a92d
shbalaku <shbalaku@cisco.com>
2019-06-16 18:49:30 +0100

    bug fix attempt

--- scripts/cleanUpPolls.js
+++ scripts/cleanUpPolls.js
@@ -37,7 +37,7 @@ function cleanUpPoll(group_name) {
     client.connect(function(err) {
       if (err) throw err;
       // get all group names in table
-      client.query('UPDATE ' + TABLE_NAME + ' SET poll_result[1]=$1, poll_timestamp=$1 WHERE group_name=$2;',
+      client.query('UPDATE ' + TABLE_NAME + ' SET poll_result=$1, poll_timestamp=$1 WHERE group_name=$2;',
       [null, group_name], function(err, res) {
         if (err) throw err;
         client.end(function(err) {
