355ff7e4f59f6dc1635f0aa8e4571aa4893043c7
shbalaku <shbalaku@cisco.com>
2019-06-18 22:45:12 +0100

    results bug fix

--- skills/utils/app-services/poll/helper.js
+++ skills/utils/app-services/poll/helper.js
@@ -228,8 +228,8 @@ function GetPollResults(group_name, user_id) {
   client.connect(function(err) {
     if (err) throw err;
     // get poll results by group_name
-    client.query('SELECT person_name AS name, poll_result AS result FROM ' + TABLE_NAME + ' WHERE group_name=$1 AND person_id!=$2 AND poll_result!=$3;',
-      [group_name, user_id, {}],
+    client.query('SELECT person_name AS name, poll_result AS result FROM ' + TABLE_NAME + ' WHERE group_name=$1 AND person_id!=$2 AND poll_in_progress=$3;',
+      [group_name, user_id, false],
       function(err, res) {
         if (err) throw err;
         client.end(function(err) {
