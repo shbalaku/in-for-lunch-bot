fd4892d19cf0814632cb40e78ed10ef2bb903c9f
shbalaku <shbalaku@cisco.com>
2019-06-22 10:52:55 +0100

    Adding debug messages

--- skills/auto.js
+++ skills/auto.js
@@ -4,14 +4,14 @@ const CommonService = require('./utils/common');
 module.exports = Controller;
 
 function Controller(controller) {
-  controller.spawn({}, function(bot) {
-    CommonService.GetMembersByGroupName('TEST')
-      .then(function(members) {
-        members.forEach(member => {
-          bot.startPrivateConversationWithPersonId(member.id, function(err, convo) {
-            convo.say('Automatically generated message');
-          });
+  var bot = controller.spawn({});
+  CommonService.GetMembersByGroupName('TEST')
+    .then(function(members) {
+      members.forEach(member => {
+        console.log(member);
+        bot.startPrivateConversationWithPersonId(member.id, function(err, convo) {
+          convo.say('Automatically generated message');
         });
       });
-  });
+    });
 }
