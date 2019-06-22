bc09ce069a0ff78963c9c03a8f0d0e2e39d96b71
shbalaku <shbalaku@cisco.com>
2019-06-22 11:34:54 +0100

    set auto poll without trigger

--- skills/auto.js
+++ skills/auto.js
@@ -5,13 +5,10 @@ module.exports = Controller;
 
 function Controller(controller) {
   var bot = controller.spawn({});
-  bot.trigger('auto_poll', [bot]);
-  controller.on('auto_poll', function(bot) {
-    CommonService.GetMembersByGroupName('TEST')
-      .then(function(members) {
-        members.forEach(member => {
-          CommonService.PollMember('AutoPoller', member.name, member.id, 'TEST', bot);
-        });
+  CommonService.GetMembersByGroupName('TEST')
+    .then(function(members) {
+      members.forEach(member => {
+        CommonService.PollMember('AutoPoller', member.name, member.id, 'TEST', bot);
       });
-  });
+    });
 }
