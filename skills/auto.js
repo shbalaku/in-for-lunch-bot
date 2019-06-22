457c1c2a20b1ff7e1ae0fa96859635f17870774f
shbalaku <shbalaku@cisco.com>
2019-06-22 11:22:26 +0100

    trying auto poll trigger

--- skills/auto.js
+++ skills/auto.js
@@ -5,10 +5,13 @@ module.exports = Controller;
 
 function Controller(controller) {
   var bot = controller.spawn({});
-  CommonService.GetMembersByGroupName('TEST')
-    .then(function(members) {
-      members.forEach(member => {
-        CommonService.PollMember('AutoPoller', member.name, member.id, 'TEST', bot);
+  bot.trigger('auto_poll', [bot]);
+  bot.on('auto_poll', function(bot) {
+    CommonService.GetMembersByGroupName('TEST')
+      .then(function(members) {
+        members.forEach(member => {
+          CommonService.PollMember('AutoPoller', member.name, member.id, 'TEST', bot);
+        });
       });
-    });
+  });
 }
